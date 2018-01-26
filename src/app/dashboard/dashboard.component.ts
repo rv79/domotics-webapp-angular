c5c2f67296943f2c6f6ebc18aea1dc0882e798b5
hmt <herve.mathonet@outlook.com>
2018-01-26 10:42:24 +0100

    .

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild('myCanvas') canvasRef: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+
+    const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+
+    // Draw the clip path that will mask everything else
+    // that we'll draw later.
+    console.log('begin drawing');
+    ctx.beginPath();
+    ctx.moveTo(0, 250);
+    ctx.lineTo(0, 500);
+    ctx.lineTo(250, 500);
+    ctx.lineTo(250, 350);
+    ctx.lineTo(350, 350);
+    ctx.stroke();
+
+    console.log('finish drawing');
+
   }
 
 }
