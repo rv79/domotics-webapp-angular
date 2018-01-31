3710b3d19eb15271382e29c5258044d6cf5d6523
hmt <herve.mathonet@outlook.com>
2018-01-31 10:16:29 +0100

    .

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,10 +20,6 @@ export class DashboardComponent implements OnInit {
 
   tabs: string[] = [];
 
-  xMargin = 90;
-  yMargin = 30;
-  scale = 0.38;
-
   icons = [
     {
       name: 'Name1',
@@ -48,8 +44,7 @@ export class DashboardComponent implements OnInit {
 
     this.drawService.drawFloor(0, this.canvas0);
     this.drawService.drawFloor(1, this.canvas1);
-
-    this.draw2();
+    this.drawService.drawFloor(2, this.canvas2);
   }
 
   onDragBegin(event, i) {
@@ -81,119 +76,5 @@ export class DashboardComponent implements OnInit {
 
     this.triggerIcon.openMenu();
   }
-
-  rectCross(ctx, x: number, y: number, w: number, h: number): void {
-    ctx.rect(x, y, w, h);
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + w, y + h);
-    ctx.moveTo(x + w, y);
-    ctx.lineTo(x, y + h);
-  }
-
-  draw2(): void {
-    const ctx: CanvasRenderingContext2D = this.canvas2.nativeElement.getContext('2d');
-
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#888888';
-    ctx.beginPath();
-    ctx.moveTo(this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.lineTo(1000 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.lineTo(1100 * this.scale + this.xMargin, 500 * this.scale + this.yMargin);
-    ctx.lineTo(1100 * this.scale + this.xMargin, 100 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 100 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-    ctx.lineTo(0 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-    ctx.lineTo(0 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-
-    ctx.moveTo(600 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 850 * this.scale + this.yMargin);
-    ctx.lineTo(1063 * this.scale + this.xMargin, 850 * this.scale + this.yMargin);
-
-    ctx.moveTo(1100 * this.scale + this.xMargin, 450 * this.scale + this.yMargin);
-    ctx.lineTo(720 * this.scale + this.xMargin, 450 * this.scale + this.yMargin);
-    ctx.lineTo(720 * this.scale + this.xMargin, 850 * this.scale + this.yMargin);
-
-    ctx.moveTo(720 * this.scale + this.xMargin, 570 * this.scale + this.yMargin);
-    ctx.lineTo(900 * this.scale + this.xMargin, 570 * this.scale + this.yMargin);
-
-    ctx.moveTo(600 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 970 * this.scale + this.yMargin);
-    ctx.lineTo(880 * this.scale + this.xMargin, 970 * this.scale + this.yMargin);
-    ctx.lineTo(880 * this.scale + this.xMargin, 1390 * this.scale + this.yMargin);
-    ctx.lineTo(1010 * this.scale + this.xMargin, 1390 * this.scale + this.yMargin);
-
-    ctx.moveTo(600 * this.scale + this.xMargin, 1100 * this.scale + this.yMargin);
-    ctx.lineTo(880 * this.scale + this.xMargin, 1100 * this.scale + this.yMargin);
-
-    ctx.moveTo(0 * this.scale + this.xMargin, 1125 * this.scale + this.yMargin);
-    ctx.lineTo(410 * this.scale + this.xMargin, 1125 * this.scale + this.yMargin);
-    ctx.lineTo(410 * this.scale + this.xMargin, 870 * this.scale + this.yMargin);
-    ctx.lineTo(480 * this.scale + this.xMargin, 870 * this.scale + this.yMargin);
-    ctx.lineTo(480 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-
-    ctx.moveTo(410 * this.scale + this.xMargin, 1125 * this.scale + this.yMargin);
-    ctx.lineTo(410 * this.scale + this.xMargin, 1300 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 1300 * this.scale + this.yMargin);
-
-    ctx.stroke();
-
-    ctx.lineWidth = 2;
-    // dressing
-    this.rectCross(ctx, 410 * this.scale + this.xMargin, 870 * this.scale + this.yMargin, 70 * this.scale, 430 * this.scale);
-
-    this.rectCross(ctx, 720 * this.scale + this.xMargin, 570 * this.scale + this.yMargin, 180 * this.scale, 70 * this.scale);
-    this.rectCross(ctx, 720 * this.scale + this.xMargin, 780 * this.scale + this.yMargin, 340 * this.scale, 70 * this.scale);
-
-    ctx.stroke();
-
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = '#EEEEEE';
-
-    ctx.beginPath();
-    // fenetre terrasse
-    ctx.moveTo(75 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-    ctx.lineTo(300 * this.scale + this.xMargin, 750 * this.scale + this.yMargin);
-    // fenetre rue
-    ctx.moveTo(300 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.lineTo(525 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.moveTo(690 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    ctx.lineTo(790 * this.scale + this.xMargin, 1500 * this.scale + this.yMargin);
-    // fenetre chambre parent
-    ctx.moveTo(600 * this.scale + this.xMargin, 175 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 475 * this.scale + this.yMargin);
-    ctx.moveTo(610 * this.scale + this.xMargin, 100 * this.scale + this.yMargin);
-    ctx.lineTo(710 * this.scale + this.xMargin, 100 * this.scale + this.yMargin);
-
-    ctx.stroke();
-
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-
-    // porte chambre parent
-    ctx.moveTo(615 * this.scale + this.xMargin, 850 * this.scale + this.yMargin);
-    ctx.lineTo(705 * this.scale + this.xMargin, 850 * this.scale + this.yMargin);
-    ctx.moveTo(720 * this.scale + this.xMargin, 660 * this.scale + this.yMargin);
-    ctx.lineTo(720 * this.scale + this.xMargin, 760 * this.scale + this.yMargin);
-
-    // porte wc
-    ctx.moveTo(690 * this.scale + this.xMargin, 970 * this.scale + this.yMargin);
-    ctx.lineTo(780 * this.scale + this.xMargin, 970 * this.scale + this.yMargin);
-
-    // porte sdb
-    ctx.moveTo(600 * this.scale + this.xMargin, 1195 * this.scale + this.yMargin);
-    ctx.lineTo(600 * this.scale + this.xMargin, 1285 * this.scale + this.yMargin);
-
-    // porte chambre rue
-    ctx.moveTo(495 * this.scale + this.xMargin, 1300 * this.scale + this.yMargin);
-    ctx.lineTo(585 * this.scale + this.xMargin, 1300 * this.scale + this.yMargin);
-
-    // porte chambre terrasse
-    ctx.moveTo(480 * this.scale + this.xMargin, 765 * this.scale + this.yMargin);
-    ctx.lineTo(480 * this.scale + this.xMargin, 855 * this.scale + this.yMargin);
-
-    ctx.stroke();
-
-  }
-
 
 }
--- src/app/data/floors.ts
+++ src/app/data/floors.ts
@@ -84,5 +84,51 @@ export const FLOORS: DrawFloor[] = [
       {type: 'exterior', x: 0, y: 740, xe: 0, ye: -30},
       {type: 'exterior', x: 0, y: -30, xe: 720, ye: -30}
     ]
+  },
+  {
+    name: 'Etage',
+    items: [
+      {type: 'wall', x: 0, y: 1500, xe: 1000, ye: 1500},
+      {type: 'wall', x: 1000, y: 1500, xe: 1100, ye: 500},
+      {type: 'wall', x: 1100, y: 500, xe: 1100, ye: 100},
+      {type: 'wall', x: 1100, y: 100, xe: 600, ye: 100},
+      {type: 'wall', x: 600, y: 100, xe: 600, ye: 750},
+      {type: 'wall', x: 600, y: 750, xe: 0, ye: 750},
+      {type: 'wall', x: 0, y: 750, xe: 0, ye: 1500},
+
+      {type: 'wall', x: 600, y: 750, xe: 600, ye: 850},
+      {type: 'wall', x: 600, y: 850, xe: 1063, ye: 850},
+      {type: 'wall', x: 1100, y: 450, xe: 720, ye: 450},
+      {type: 'wall', x: 720, y: 450, xe: 720, ye: 850},
+      {type: 'wall', x: 720, y: 570, xe: 900, ye: 570},
+      {type: 'wall', x: 600, y: 1500, xe: 600, ye: 970},
+      {type: 'wall', x: 600, y: 970, xe: 880, ye: 970},
+      {type: 'wall', x: 880, y: 970, xe: 880, ye: 1390},
+      {type: 'wall', x: 880, y: 1390, xe: 1010, ye: 1390},
+      {type: 'wall', x: 600, y: 1100, xe: 880, ye: 1100},
+      {type: 'wall', x: 0, y: 1125, xe: 410, ye: 1125},
+      {type: 'wall', x: 410, y: 1125, xe: 410, ye: 870},
+      {type: 'wall', x: 410, y: 870, xe: 480, ye: 870},
+      {type: 'wall', x: 480, y: 870, xe: 480, ye: 750},
+      {type: 'wall', x: 410, y: 1125, xe: 410, ye: 1300},
+      {type: 'wall', x: 410, y: 1300, xe: 600, ye: 1300},
+
+      {type: 'door', x: 615, y: 850, xe: 705, ye: 850},
+      {type: 'door', x: 720, y: 660, xe: 720, ye: 760},
+      {type: 'door', x: 690, y: 970, xe: 780, ye: 970},
+      {type: 'door', x: 600, y: 1195, xe: 600, ye: 1285},
+      {type: 'door', x: 495, y: 1300, xe: 585, ye: 1300},
+      {type: 'door', x: 480, y: 765, xe: 480, ye: 855},
+
+      {type: 'window', x: 75, y: 750, xe: 300, ye: 750},
+      {type: 'window', x: 300, y: 1500, xe: 525, ye: 1500},
+      {type: 'window', x: 690, y: 1500, xe: 790, ye: 1500},
+      {type: 'window', x: 600, y: 175, xe: 600, ye: 475},
+      {type: 'window', x: 610, y: 100, xe: 710, ye: 100},
+
+      {type: 'cabinet', x: 410, y: 870, xe: 480, ye: 1300},
+      {type: 'cabinet', x: 720, y: 570, xe: 900, ye: 640},
+      {type: 'cabinet', x: 720, y: 780, xe: 1060, ye: 850}
+    ]
   }
 ];
